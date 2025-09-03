4172d1d656645f801d0a8e8951412e7232e915a9
Daniel Canoy <canoydaniel06@gmail.com>
2025-09-03 23:35:04 +0800

    configuring next to allow images from google and github

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
         hostname: "platform-lookaside.fbsbx.com",
       },
     ],
